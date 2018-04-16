f5409691dd9282c96c22e1da2d157564fecf781e
李明 <672486796@qq.com>
2018-04-16 21:35:39 +0800

    ad

--- src/components/page/person/PersonManage.vue
+++ src/components/page/person/PersonManage.vue
@@ -11,22 +11,22 @@
 				</el-breadcrumb>
 			</div>
 			<div class="data-box">
-				<el-row class="tac">
-					<el-col :span="2">
-					    <el-menu default-active="1"  class="el-menu-vertical-demo">
-						    <el-menu-item index="1" @click="loadData(1)">人员管理</el-menu-item>
-						    <el-menu-item index="2" @click="loadData(2)">部门管理</el-menu-item>
-					    </el-menu>
-					</el-col>
-					<!--人员管理-->
-					<el-col v-show="index==1" :offset="1" :span="21">
-						<emp-list></emp-list>
-					</el-col>
-					<!--部门管理-->
-					<el-col v-show="index==2" :offset="1" :span="21">
-						<dept-list></dept-list>
-					</el-col>
-				</el-row>
+				<table>
+					<tr>
+						<td style="width: 150px;" valign="top">
+							<el-menu default-active="1"  class="el-menu-vertical-demo">
+							    <el-menu-item index="1" @click="loadData(1)">人员管理</el-menu-item>
+							    <el-menu-item index="2" @click="loadData(2)">部门管理</el-menu-item>
+						    </el-menu>
+						</td>
+						<td valign="top" style="padding-left: 15px;padding-top: 10px;">
+							<!--人员管理-->
+							<emp-list v-show="index==1"></emp-list>
+							<!--部门管理-->
+							<dept-list v-show="index==2"></dept-list>
+						</td>
+					</tr>
+				</table>
 			</div>
 		</div>
 	</div>
@@ -58,9 +58,6 @@
 </script>
 
 <style scoped>
-	#personManage{
-		min-width: 1366px;
-	}
 	#personManage .data-box{
 		margin: 0 20px;
 	}
